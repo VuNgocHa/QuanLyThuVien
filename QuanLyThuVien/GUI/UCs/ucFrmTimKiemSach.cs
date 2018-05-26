c55c959a0b093d1a976e2fde135d52807248ce63
lenguyenminhhieu <tertium97@gmail.com>
2018-05-26 15:28:57 +0700

    tim kiem sach + tro giup

--- QuanLyThuVien/GUI/UCs/ucFrmTimKiemSach.cs
+++ QuanLyThuVien/GUI/UCs/ucFrmTimKiemSach.cs
@@ -8,11 +8,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QuanLyThuVien.BUS;
+using QuanLyThuVien.DTO;
 
 namespace QuanLyThuVien.GUI.UCs
 {
     public partial class ucFrmTimKiemSach : DevExpress.XtraEditors.XtraUserControl
     {
+        Sach_BUS sachBUS = new Sach_BUS();
         public ucFrmTimKiemSach()
         {
             InitializeComponent();
@@ -20,12 +23,31 @@ namespace QuanLyThuVien.GUI.UCs
 
         private void txtTimKiem_EditValueChanged(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            if (txtTimKiem.Text == "")
+            {
+                gridControl1.DataSource = sachBUS.GetList();
+
+            }
+            else
+            {
+                if (rdoBtnMaSach.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "MaSach");
+                else if (rdoBtnNhaXuatBan.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "NhaXuatBan");
+                else if (rdoBtnTacGia.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "TacGia");
+                else if (rdoBtnTenSach.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "TenSach");
+                else if (rdoBtnTheLoai.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "TheLoai");
+                else if (rdoBtnTinhTrang.Checked == true)
+                    gridControl1.DataSource = sachBUS.TimKiem(txtTimKiem.Text, "TinhTrang");
+            }
         }
 
         private void ucFrmTimKiemSach_Load(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            gridControl1.DataSource = sachBUS.GetList();
         }
     }
 }
